d326b0beaeb95f6a92eb3745fed2eb7c40cc3f99
Luisa <lhurtado372@gmail.com>
2021-05-11 21:20:24 -0500

    reto 3 superado

--- src/App.js
+++ src/App.js
@@ -2,13 +2,31 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      textAreaValue: "",
+      count: 0
+
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+  
+
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" value={this.state.textAreaValue} onChange={this.handleChange} ></textarea>
+        <div className="counter">{this.state.count}</div>
       </div>
     );
+  }
+
+  handleChange (event) {
+    this.setState({
+      textAreaValue: event.target.value,
+      count: event.target.value.length
+    })
   }
 }
 
